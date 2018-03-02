ddc6aebb4f21c33cbfbe7cd4225204e429396a6e
qwersem <prodigy.94@mail.ru>
2018-03-02 18:11:23 +0700

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,18 +1,9 @@
 
 public class MainClass {
 
-	public MainClass() {
-		// TODO Auto-generated constructor stub
-	}
-
+	public MainClass() {}
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		int respCount = 200;
-		
 		DiagramRadarSignal_2 diagRadarSignal = new DiagramRadarSignal_2();
-		//double otklik = diagRadarSignal.otklik.signal[];
-		
-		//DiagramDetectRespiration diagramResp = new DiagramDetectRespiration(diagRadarSignal.otklik.signal[respCount], diagRadarSignal.sizeDetectResp);
-		
 	}
 }
