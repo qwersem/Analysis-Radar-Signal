7f81563ea7d845b9a805662ee917bd9e69f6bf62
qwersem <prodigy.94@mail.ru>
2018-03-02 18:09:59 +0700

    Update FluctuationSignal.java

--- FluctuationSignal.java
+++ FluctuationSignal.java
@@ -1,11 +1,8 @@
 
 public class FluctuationSignal extends FrameSignal {
-	
 	public FluctuationSignal(int firstCount, int halfSize, double Amplitude) {
-
 		int size = 2*halfSize;
 		signal = new double[size];
-		
 		for (int i = 0; i < halfSize; i++)
 			signal[i] = Amplitude*Math.sin(Math.PI*i/size);
 		for (int i = halfSize; i < size; i++)
